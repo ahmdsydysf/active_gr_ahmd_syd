6b6d5a913122339546a9b4380e1d99d3b6095286
sabreenMohammed107 <sabreenm312@gmail.com>
2022-04-29 21:03:59 +0200

    hh

--- resources/views/dash_site/company/edit.blade.php
+++ resources/views/dash_site/company/edit.blade.php
@@ -28,42 +28,42 @@
 
             <div class="form-group">
                 <label>عن الشركة بالانجليزي</label>
-                <textarea  name="overview_en" class="form-control" rows="3" placeholder="النص">{{$row->overview_en}}</textarea>
+                <textarea  name="overview_en" class="form-control summernote" rows="3" placeholder="النص">{{$row->overview_en}}</textarea>
             </div>
             @error('overview_en')
             <div class="alert alert-danger">{{ $message }}</div>
             @enderror
             <div class="form-group">
                 <label>عن الشركة بالعربي</label>
-                <textarea  name="overview_ar" class="form-control" rows="3" placeholder="النص">{{$row->overview_ar}}</textarea>
+                <textarea  name="overview_ar" class="form-control summernote" rows="3" placeholder="النص">{{$row->overview_ar}}</textarea>
             </div>
             @error('overview_ar')
             <div class="alert alert-danger">{{ $message }}</div>
             @enderror
             <div class="form-group">
                 <label>الهدف انجليزي</label>
-                <textarea  name="mission_en" class="form-control" rows="3" placeholder="النص">{{$row->mission_en}}</textarea>
+                <textarea  name="mission_en" class="form-control summernote" rows="3" placeholder="النص">{{$row->mission_en}}</textarea>
             </div>
             @error('mission_en')
             <div class="alert alert-danger">{{ $message }}</div>
             @enderror
             <div class="form-group">
                 <label>الهدف عربي</label>
-                <textarea  name="mission_ar" class="form-control" rows="3" placeholder="النص">{{$row->mission_ar}}</textarea>
+                <textarea  name="mission_ar" class="form-control summernote" rows="3" placeholder="النص">{{$row->mission_ar}}</textarea>
             </div>
             @error('mission_ar')
             <div class="alert alert-danger">{{ $message }}</div>
             @enderror
             <div class="form-group">
                 <label>الرؤية انجليزي</label>
-                <textarea  name="vision_en" class="form-control" rows="3" placeholder="النص">{{$row->vision_en}}</textarea>
+                <textarea  name="vision_en" class="form-control summernote" rows="3" placeholder="النص">{{$row->vision_en}}</textarea>
             </div>
             @error('vision_en')
             <div class="alert alert-danger">{{ $message }}</div>
             @enderror
             <div class="form-group">
                 <label>الرؤية عربي</label>
-                <textarea  name="vision_ar" class="form-control" rows="3" placeholder="النص">{{$row->vision_ar}}</textarea>
+                <textarea  name="vision_ar" class="form-control summernote" rows="3" placeholder="النص">{{$row->vision_ar}}</textarea>
             </div>
             @error('vision_ar')
             <div class="alert alert-danger">{{ $message }}</div>
